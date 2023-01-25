8ed4ff94d5461f59adb2baf82dfab03cfc1a9b26
Lee Grobler <lee@mfg.gg>
2023-01-25 09:53:50 +0200

    added linting with prettier

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
   <h1 class="text-3xl text-pink-700 font-bold mt-5">App File</h1>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
