90fdb22b19a0f963deff1e6b4e396006963e804c
Lee Grobler <lee@mfg.gg>
2023-01-26 09:26:09 +0200

    some default assets cleanup. wired up tailwindui. started building out the layout and home page.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 // tailwind.config.js
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: 'media', // or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   variants: {
     extend: {},
