bd61237fa9b3cd323deb2dbdcffa6a64d8e20992
Lee Grobler <lee@mfg.gg>
2023-02-27 09:01:55 +0200

    started working on near wallet integration.

---
+++ src/components/Auth/NearWallet.vue
@@ -0,0 +1,58 @@
+<script setup>
+  import { ref } from 'vue'
+  import { ArrowPathIcon } from '@heroicons/vue/24/outline'
+  import { ExclamationTriangleIcon } from '@heroicons/vue/20/solid'
+  import { keyStores } from 'near-api-js'
+
+  import router from '../../router'
+  import { useUserStore } from '../../stores/user'
+  import { useAppManagerStore } from '../../stores/app-manager'
+
+  const props = defineProps({ loading: Boolean })
+  const emit = defineEmits(['toggleLoading'])
+
+  const userStore = useUserStore()
+  const appManagerStore = useAppManagerStore()
+
+  const nearLogin = () => {}
+
+  // const submit = async () => {
+  //   try {
+  //     emit('toggleLoading')
+  //     // const token = await ethereum.request({ method: 'eth_requestAccounts' })
+  //     // await userStore.exchangeMetamaskTokenForJwt({ token: token[0] })
+  //     appManagerStore.showAlert({ color: 'success', text: "You've successfully been logged in" })
+  //     router.push('/dashboard')
+  //   } catch (err) {
+  //     console.log('err:', err)
+  //     appManagerStore.showAlert({ color: 'error', text: err.message })
+  //   }
+  //   emit('toggleLoading')
+  // }
+</script>
+
+<template>
+  <!-- 
+    NEAR colors:
+    ----------------
+    light: #1E1E1E
+    dark: #b0b0b0
+  -->
+
+  <button
+    type="button"
+    :disabled="loading"
+    :class="`inline-flex items-center justify-center rounded-md border border-transparent px-4 py-2 font-normal shadow-sm
+      focus:outline-none focus:ring-2 focus:ring-offset-2 ${!loading ? 'hover:bg-[#000000]' : ''} text-2xl text-white
+      ${!loading ? 'bg-[#1E1E1E]' : 'bg-gray-400'} w-full`"
+    @click="nearLogin"
+  >
+    <ArrowPathIcon v-if="loading" class="h-5 w-5 animate-spin" />
+    <div v-else class="flex items-center gap-4">
+      <img src="../../assets/images/near-icon.jpg" alt="near wallet icon" class="w-8" />
+      <span>NEAR Wallet</span>
+    </div>
+  </button>
+</template>
+
+<style></style>
--- src/components/Signin/Index.vue
+++ src/components/Signin/Index.vue
@@ -3,7 +3,8 @@
 
   import Divider from '../Layout/Divider.vue'
   import Metamask from '../Auth/Metamask.vue'
-  import Google from '../Auth/Google.vue'
+  import NearWallet from '../Auth/NearWallet.vue'
+  // import Google from '../Auth/Google.vue'
   import EmailPassword from './EmailPassword.vue'
 
   const loading = ref(false)
@@ -11,12 +12,13 @@
 
 <template>
   <section class="rounded-lg bg-white shadow p-6 lg:mx-52 flex flex-col gap-4">
-    <Metamask :loading="loading" @toggleLoading="loading = !loading" />
-    <Google :loading="loading" @toggleLoading="loading = !loading" />
+    <Metamask :loading="loading" @toggleLoading="() => (loading = !loading)" />
+    <NearWallet :loading="loading" @toggleLoading="() => (loading = !loading)" />
+    <!-- <Google :loading="loading" @toggleLoading="() => loading = !loading" /> -->
 
     <Divider text="OR" classes="my-4" />
 
-    <EmailPassword :loading="loading" @toggleLoading="loading = !loading" />
+    <EmailPassword :loading="loading" @toggleLoading="() => (loading = !loading)" />
 
     <div class="text-center">
       <router-link to="forgot-password" class="text-[#3366CC] hover:text-[#5b80ca] hover:underline">
