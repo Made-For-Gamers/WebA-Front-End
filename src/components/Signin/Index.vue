b04e1d3e8540a46fb091444b8f075aab51a6d0ea
JP van Loggerenberg <jploggies@gmail.com>
2023-03-13 08:08:43 +0100

    NEAR Wallet Login changes

--- src/components/Signin/Index.vue
+++ src/components/Signin/Index.vue
@@ -12,11 +12,11 @@
 
 <template>
   <section class="rounded-lg bg-white shadow p-6 lg:mx-52 flex flex-col gap-4">
-    <!-- <Metamask :loading="loading" @toggleLoading="() => (loading = !loading)" />
+    <Metamask :loading="loading" @toggleLoading="() => (loading = !loading)" />
     <NearWallet :loading="loading" @toggleLoading="() => (loading = !loading)" />
-    <Google :loading="loading" @toggleLoading="() => loading = !loading" />
+    <!-- <Google :loading="loading" @toggleLoading="() => loading = !loading" />-->
 
-    <Divider text="OR" classes="my-4" /> -->
+    <Divider text="OR" classes="my-4" /> 
 
     <EmailPassword :loading="loading" @toggleLoading="() => (loading = !loading)" />
 
